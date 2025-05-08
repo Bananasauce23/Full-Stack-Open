2238f2e8b98d6f79708140c7ec411e0ffa7dc323
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-05-08 11:37:53 +0300

    Add files via upload

---
+++ Osa 2/2.7/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    handleCheck()
+    handleNameChange()
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleCheck = () => {
+    setPersons(persons.concat({name:newName}))
+  }
+
+return (
+  <div>
+    <h2>Phonebook</h2>
+    <form onSubmit={addName}>
+      <div>
+        Name: <input id="name" onChange={handleNameChange}/>
+      </div>
+      <div>
+        <button type="submit">Add</button>
+      </div>
+    </form>
+    <h2>Numbers</h2>
+    {persons.map(person => <div key={person.name}>{person.name}</div>)}
+  </div>
+  )
+}
+
+export default App
