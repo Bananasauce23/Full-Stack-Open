245ff04ac35fc3264d06b7317954495c7b5a4406
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-05-28 13:38:24 +0300

    Add files via upload

---
+++ Osa 2/2.12/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
+
+  const namesToShow = showAll
+  ? persons
+  : persons.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    persons.some(person => person.name == newName)
+    ? alert(`${newName} is already added to phonebook`) 
+    : axios 
+    .post('http://localhost:3001/persons', personObject)
+    .then(response => {
+      setPersons(persons.concat(response.data))
+    })
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchCheck = (event) => {
+    if (event.target.value.length >= 1) {
+      setShowAll(false)
+      setSearch(event.target.value)
+    }
+    else {
+      setShowAll(true)
+    }
+  }
+
+return (
+  <div>
+    <h2>Phonebook</h2>
+    <Filter handleSearchCheck={handleSearchCheck}/>
+
+    <h2>Add new</h2>
+    <PersonForm 
+    addName={addName}
+    handleNameChange={handleNameChange}
+    handleNumberChange={handleNumberChange}/>
+
+    <h2>Numbers</h2>
+    <Persons namesToShow={namesToShow}/>
+  </div>
+  )
+}
+
+export default App
