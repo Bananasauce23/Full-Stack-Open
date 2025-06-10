827ea515f89e403021a04e65f8cdce5c16ce7f4d
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-10 15:40:34 +0300

    Update index.js

--- Osa 3/3.5/index.js
+++ Osa 3/3.5/index.js
@@ -54,14 +54,6 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
     const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({error: "name or number missing"})
-    }
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({error: "name must be unique"})
-    }
-
     const person = {
         id: Math.floor(Math.random() * 100000).toString(),
         name: body.name,
