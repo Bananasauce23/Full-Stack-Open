ddcdc34d96efe56a85ead1949890f16b2b29c2c1
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-13 14:56:13 +0300

    Add files via upload

--- Osa 3/3.10 - Puhelinluettelo internetiin/backend/index.js
+++ Osa 3/3.10 - Puhelinluettelo internetiin/backend/index.js
@@ -1,10 +1,8 @@
 const express = require("express")
 const morgan = require("morgan")
-const cors = require("cors")
 const app = express()
 morgan.token("body", (request) => JSON.stringify(request.body))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
-app.use(cors())
 app.use(express.static("dist"))
 
 let persons = [
