40207393f02323ec824272ba2d4f7dd37f1d62c1
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-14 17:54:36 +0300

    Add files via upload

--- Osa 3/3.10 - Puhelinluettelo internetiin/backend/index.js
+++ Osa 3/3.10 - Puhelinluettelo internetiin/backend/index.js
@@ -1,37 +1,19 @@
+require('dotenv').config()
 const express = require("express")
+const Person = require('./models/person')
 const morgan = require("morgan")
+const mongoose = require('mongoose')
 const app = express()
 morgan.token("body", (request) => JSON.stringify(request.body))
+
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 app.use(express.static("dist"))
-
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-
 app.use(express.json())
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get("/info", (request, response) => {
@@ -76,7 +58,7 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
