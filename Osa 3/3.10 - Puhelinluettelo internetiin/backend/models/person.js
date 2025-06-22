da04ddab54f20c4dc8f4df103ef9aaa5203e96fc
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-22 14:11:21 +0300

    Add files via upload

--- Osa 3/3.10 - Puhelinluettelo internetiin/backend/models/person.js
+++ Osa 3/3.10 - Puhelinluettelo internetiin/backend/models/person.js
@@ -4,44 +4,44 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log("Connecting to", url)
+console.log('Connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log("Connected to MongoDB")
-    })
-    .catch((error) => {
-        console.log("Error connecting to MongoDB:", error.message)
-    })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 const numberValidator = [
-    {
-        validator: function(v) {
-            if (!v || v.length < 8) return false
-            return /^\d{2,3}-\d{5,}$/.test(v)
-        },
-        message: props => `${props.value} is not a valid number. Format: XX-XXXXX or XXX-XXXXX`
-    }
+  {
+    validator: function(v) {
+      if (!v || v.length < 8) return false
+      return /^\d{2,3}-\d{5,}$/.test(v)
+    },
+    message: props => `${props.value} is not a valid number. Format: XX-XXXXX or XXX-XXXXX`
+  }
 ]    
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        required: true,
-        validate: numberValidator
-    }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: numberValidator
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-module.exports = mongoose.model("Person", personSchema)
+module.exports = mongoose.model('Person', personSchema)
