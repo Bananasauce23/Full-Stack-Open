e9862b58800460d13f29c6a603a856a54dbcb7e7
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-25 15:32:42 +0300

    Add files via upload

---
+++ Osa 4/4.10/tests/blog_api.test.js
@@ -0,0 +1,69 @@
+const { test, beforeEach, after} = require('node:test');
+const assert = require('node:assert');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+
+const api = supertest(app);
+
+const initialBlogs = [
+    {
+        title: 'Jokublogi1',
+        author: 'Joku tekijä',
+        url: 'http://esim.com/1',
+        likes: 4
+    },
+    {
+        title: 'Jokutoinen2',
+        author: 'Joku toinen',
+        url: 'http://jotain.com/2',
+        likes: 3
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(initialBlogs);
+})
+
+test('blogs are returned as json and correct amount', async () => {
+    const response = await api.get('/api/blogs');
+    assert.strictEqual(response.status, 200);
+    assert.match(response.headers['content-type'], /application\/json/);
+    assert.strictEqual(response.body.length, initialBlogs.length);
+})
+
+test('blogs have their id fields named id', async () => {
+    const response = await api.get('/api/blogs');
+    response.body.forEach(blog => {
+        assert.notStrictEqual(blog.id, undefined);
+        assert.strictEqual(blog._id, undefined);
+    })
+})
+
+test('valid blog can be added', async () => {
+    const newBlog = {
+        title: 'New Blog',
+        author: "Test",
+        url: "http://test.com",
+        likes: 7
+    };
+
+    const blogsAtStart = await api.get('/api/blogs');
+    const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await api.get('/api/blogs');
+    assert.strictEqual(blogsAtEnd.body.length, blogsAtStart.body.length + 1);
+
+    const titles = blogsAtEnd.body.map(b => b.title);
+    assert.ok(titles.includes(newBlog.title));
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
