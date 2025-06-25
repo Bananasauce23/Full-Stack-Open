433951d15929f1a80752bc2043413382aba5cedd
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-25 15:50:22 +0300

    Add files via upload

---
+++ Osa 4/4.12 - Blogilistan testit osa 5/controllers/blogs.js
@@ -0,0 +1,19 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const { title, url } = request.body;
+    if (!title || !url) {
+        return response.status(400).end();
+    }
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+})
+
+module.exports = blogsRouter
