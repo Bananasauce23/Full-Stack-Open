39960eb0f688d315a290d9d563dd6a23815a746a
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-25 15:20:42 +0300

    Add files via upload

---
+++ Osa 4/4.9/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
+    mongoose.connect(process.env.TEST_MONGODB_URI || process.env.MONGODB_URI)
+}
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
