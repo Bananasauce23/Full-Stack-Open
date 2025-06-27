edca68a09f4641d8f9615c0a029c5409ce2db0bd
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-06-27 17:22:44 +0300

    Add files via upload

---
+++ Osa 4/4.19/controllers/blogs.js
@@ -0,0 +1,79 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 });
+    response.json(blogs);
+});
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '');
+    }
+    return null
+}
+
+blogsRouter.post('/', async (request, response) => {
+    const token = getTokenFrom(request);
+    
+    if (!token) {
+        return response.status(401).json({ error: 'token missing' });
+    }
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.SECRET);
+    } catch (error) {
+        return response.status(401).json({ error: 'token invalid'});
+    }
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' });
+    }
+
+    const user = await User.findById(decodedToken.id);
+    if (!user) {
+        return response.status(400).json({ error: 'UserId missing or not valid'});
+    }
+
+    const { title, url, likes } = request.body;
+    const blog = new Blog({
+        title,
+        author: user.name,
+        url,
+        likes: likes || 0,
+        user: user._id
+    });
+
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+    response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+    if (!mongoose.Types.ObjectId.isValid(request.params.id)) {
+        return response.status(400).json({ error: 'malformatted id' });
+    }
+    await Blog.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const { likes } = request.body;
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        { likes },
+        { new: true, runValidators: true, context: 'query' }
+    );
+    if (updatedBlog) {
+        response.json(updatedBlog);
+    }
+    else {
+        response.status(404).end();
+    }
+})
+
+module.exports = blogsRouter;
