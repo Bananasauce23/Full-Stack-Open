8846932130bd32af5790879f93868c310e57c8e1
VH <114520232+Bananasauce23@users.noreply.github.com>
2025-09-10 13:56:58 +0300

    Add files via upload

---
+++ Osa 6/6.4/src/App.jsx
@@ -0,0 +1,27 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const handleVote = (id) => {
+    dispatch(voteAnecdote(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteForm />
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          {anecdote.content} has {anecdote.votes}
+          <button onClick={() => handleVote(anecdote.id)}>vote</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
